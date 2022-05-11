0cc83f66d27c6cfa0f0253dd4649a5fe29dc4b4d
Akinwole Samuel <samuelakinwole@gmail.com>
2022-05-11 10:59:11 +0100

    First commit

---
+++ Lab_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Samuel, I am in backEnd Development Track of Wella Schools");
+            Console.WriteLine("I'm learning BackEnd Development so i can know how to write clean and efficient codes for the proper functionality of a website and/or Application");
+            Console.WriteLine("I'm also learning because I love Programming");
+
+            Console.ReadKey();
+        }
+    }
+}
